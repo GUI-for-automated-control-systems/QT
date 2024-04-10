70401c56d66879f8eb0960f797db1cd0266f6238
NeZorinEgor <zorin-ep@mail.ru>
2024-04-10 16:36:02 +0800

    feat: realize sendToClient()

---
+++ serverSockert/server.cpp
@@ -0,0 +1,55 @@
+#include <server.h>
+
+Server::Server()
+{
+    if(this->listen(QHostAddress::Any, 2323))
+    {
+        qDebug() << "start";
+    }
+    else
+    {
+        qDebug() << "Server error";
+    }
+}
+
+void Server::incomingConnection(qintptr socketDescriptor)
+{
+    socket = new QTcpSocket;
+    socket->setSocketDescriptor(socketDescriptor);
+    connect(socket, &QTcpSocket::readyRead, this, &Server::slotReadyRead);
+    connect(socket, &QTcpSocket::disconnected, socket, &QTcpSocket::deleteLater);
+
+    Sockets.push_back(socket);
+    qDebug() << "Client connected successfuly";
+}
+
+void Server::slotReadyRead()
+{
+    socket = (QTcpSocket*)sender();
+    QDataStream in(socket);     // работа с данными сокета
+    in.setVersion(QDataStream::Qt_6_2);
+    if (in.status() == QDataStream::Ok)
+    {
+        qDebug() << "Read data";
+        QString string;
+        in >> string;
+        qDebug() << string;
+
+    }
+    else
+    {
+        qDebug() << "Stream error!";
+    }
+}
+
+void Server::sendToClient(QString message)
+{
+    Data.clear();
+    QDataStream out(&Data, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_6_2);
+    out << message;
+    socket->write(Data);
+}
+
+
+
--- serverSockert/server.h
+++ serverSockert/server.h
@@ -1,4 +1,25 @@
 #ifndef SERVER_H
 #define SERVER_H
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QVector>
+
+class Server : public QTcpServer
+{
+    Q_OBJECT
+
+public:
+    Server();
+    QTcpSocket * socket;
+
+private:
+    QVector <QTcpSocket*> Sockets;
+    QByteArray Data; // client <=> server
+    void sendToClient(QString message);
+
+public slots:
+    void incomingConnection(qintptr socketDescriptor);
+    void slotReadyRead();
+};
 
 #endif // SERVER_H
